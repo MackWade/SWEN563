5b4f640049adc4474337dfb96ff5d85ae4405a7d
MackWade <kinkinwade@gmail.com>
2022-08-28 20:27:16 -0700

    project part 1 completed

--- P0-LED/Debug/Core/Src/button.d
+++ P0-LED/Debug/Core/Src/button.d
@@ -1,2 +1,15 @@
-Core/Src/button.o: ../Core/Src/button.c ../Core/Inc/stm32l476xx.h
-../Core/Inc/stm32l476xx.h:
+Core/Src/button.o: ../Core/Src/button.c \
+ ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h
+../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h:
+../Drivers/CMSIS/Include/core_cm4.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Include/mpu_armv7.h:
+../Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
